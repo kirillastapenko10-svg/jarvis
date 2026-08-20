5c452f365890483fe677c9eb08b0eea961a202fb
kirillastapenko10-svg <kirill.astapenko10@gmail.com>
2026-08-20 18:14:22 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
-const CACHE = "jarvis-v1";
+const CACHE = "jarvis-v2";
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE).then(cache =>
       cache.addAll([
@@ -12,12 +14,37 @@ self.addEventListener("install", event => {
   );
 });
 
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request).catch(() =>
-        caches.match("./index.html")
+    fetch(event.request)
+      .then(response => {
+
+        if (event.request.method === "GET") {
+
+          const copy = response.clone();
+
+          caches.open(CACHE).then(cache => {
+            cache.put(event.request, copy);
+          });
+
+        }
+
+        return response;
+      })
+      .catch(() =>
+        caches.match(event.request)
       )
-    )
   );
 });
